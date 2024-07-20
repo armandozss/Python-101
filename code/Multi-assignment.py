10b3260789868066b60fe1a6631418e76ec52db2
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 17:37:10 -0400

    Create Multi-assignment.py

---
+++ code/Multi-assignment.py
@@ -0,0 +1,33 @@
+bre = "----------------------------------------------"
+end = "This is all of multiple assignment"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# Without multiple assignment
+age = 14
+name = "befuzs"
+last_name = "shisa"
+
+print(age)
+print(name)
+print(last_name)
+
+# Break
+print(" ")
+print(bre)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# With multiple assignment
+MA = age, name, last_name = 14, "befuzs", "shisa"
+
+print(MA)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

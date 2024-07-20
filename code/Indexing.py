430c271effad4fb140dc366dc2fbe6b0ed4831a9
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 19:10:29 -0400

    Create Indexing.py

---
+++ code/Indexing.py
@@ -0,0 +1,29 @@
+bre = "----------------------------------------------"
+end = "This is all of indexing"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+name = "Befuzs Shisazs "
+
+if(name[0].isupper()):
+    name = name.lower()
+print(name)
+
+# Break
+print(" ")
+print(bre)
+
+first_name = name[:5].upper()
+last_name = name[7:-1].lower()
+
+print(first_name + " " + last_name)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+
+#Made by danilo.zs

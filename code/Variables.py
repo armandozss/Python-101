fee4f8be3751860f0046d8a5d3394e7e3fee44f0
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 17:35:16 -0400

    Create Variables.py

---
+++ code/Variables.py
@@ -0,0 +1,67 @@
+bre = "----------------------------------------------"
+end = "This is all of basic variables"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# Strings (str)
+name = "befuzs"
+print("Hello" + " " + name + "!!!")
+
+first_name = "befuzs"
+second_name = "shisa"
+full_name = first_name + " " + second_name
+print("Welcome" + " " + full_name)
+
+# Datatype str
+print(type(name))
+
+# Break
+print(" ")
+print(bre)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# Integer (int)
+age = 14
+age += 20
+print("Your age in 20 years is:" + " " + str(age))
+
+# Datatype int
+print(type(age))
+
+# Break
+print(" ")
+print(bre)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# Float
+height = 175.6
+print("Your height is" + " " + str(height) + "cm")
+
+# Datatype (float)
+print(type(height))
+
+# Break
+print(" ")
+print(bre)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# Boolean (bool)
+me = True
+robot = False
+print("Im a human:" + " " + str(me))
+print("Is a robot a human?" + " " + str(robot))
+
+# Datatype (bool)
+print(type(me))
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

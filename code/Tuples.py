1f9118e2f7cb38dc397f3fb22ae387f370548a5d
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 18:31:04 -0400

    Create Tuples.py

---
+++ code/Tuples.py
@@ -0,0 +1,32 @@
+bre = "----------------------------------------------"
+end = "This is all of Tuples"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+student = ("Befuzs", 14, "male")
+
+print(student.index("male"))
+
+# Break
+print(" ")
+print(bre)
+
+if "Befuzs" in student:
+    print("Befuzs is here")
+
+# Break
+print(" ")
+print(bre)
+
+for x in student:
+    print(x, end=" ")
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+
+#Made by danilo.zs

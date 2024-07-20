ed9919e232c2df46b7125e6c0aa03d3b7330e00f
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 18:28:09 -0400

    Create Lists.py

---
+++ code/Lists.py
@@ -0,0 +1,29 @@
+bre = "----------------------------------------------"
+end = "This is all of Lists"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+food = ["Pizza", "Sphaguetti", "Hot dog", "pudding"]
+
+food[-1] = "sushi"
+print(food)
+
+food.append("Ice cream")
+print(food)
+
+food.remove("Sphaguetti")
+print(food)
+
+print(food[-1])
+
+for x in food:
+    print(x, end=" ")
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

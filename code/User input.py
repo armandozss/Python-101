543e743b2b1fdc3728c190dbdadf212d408d6f7f
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 17:44:24 -0400

    Create User input.py

---
+++ code/User input.py
@@ -0,0 +1,21 @@
+bre = "----------------------------------------------"
+end = "This is all of user input"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+height = float(input("What is your height?: "))
+
+print("Hello " + name)
+print("Hi your age is " + str(age) + " years old")
+print("So your height is " + str(height))
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

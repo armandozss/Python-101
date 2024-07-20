f0fb9845b686f1249985ab6a3b58a1a266a44f5e
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 18:29:09 -0400

    Create 2D list.py

---
+++ code/2D list.py
@@ -0,0 +1,21 @@
+bre = "----------------------------------------------"
+end = "This is all of 2D lists"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+drinks = ["Coffee", "Tea", "Soda", "Lemonade"]
+food = ["Pizza", "Sushi", "Spaguetti", "Hot dog"]
+dessert = ["Ice cream", "Pudding", "Cake"]
+
+menu = [drinks, food, desert]
+for x in menu:
+    print(x)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

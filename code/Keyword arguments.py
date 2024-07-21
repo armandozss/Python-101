d85dd383f4f61da07d700043ba1dc4d13dffc132
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 21:44:35 -0400

    Create Keyword arguments.py

---
+++ code/Keyword arguments.py
@@ -0,0 +1,18 @@
+bre = "----------------------------------------------"
+end = "This is all of keyword arguments"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+def cheers(first, middle, last):
+    print("Hello " + first + " " + middle + " " + last + " ")
+
+cheers(middle="Flaito", first="Befuzs", last="Shisazs")
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

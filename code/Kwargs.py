6e7f06b1af03fb15d22c23d899a72a3cd46b1ea8
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 22:25:42 -0400

    Create Kwargs.py

---
+++ code/Kwargs.py
@@ -0,0 +1,20 @@
+bre = "----------------------------------------------"
+end = "This is all of kwargs"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+def dictionary(**kwargs):
+    print("Hello ", end="")
+    for key, value in kwargs.items():
+        print(value, end="")
+
+dictionary(tittle="Mr. ", first="befuzs ", last="shisazs")
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs

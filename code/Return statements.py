4800ab5853408fe83d6bb0d886d9358bae35dae1
Armando Mendieta <64376011+armandozss@users.noreply.github.com>
2024-07-20 21:40:42 -0400

    Create Return statements.py

---
+++ code/Return statements.py
@@ -0,0 +1,24 @@
+bre = "----------------------------------------------"
+end = "This is all of return statement"
+
+#-----------------------------------------------------------------------------------------------------------------
+
+def multiply(number1, number2):
+    results = number1 * number2
+    return results
+
+number1 = int(input("Introduce a number: "))
+number2 = int(input("Introduce another number: "))
+
+x = multiply(number1, number2)
+
+print(x)
+
+#-----------------------------------------------------------------------------------------------------------------
+
+# End
+print(" ")
+print(bre)
+print(end)
+
+#Made by danilo.zs
